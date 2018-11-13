4515190b18d4aef42ba8e75ef7c8994d9ebb56ef
刘子西 <Liuzx1337@163.com>
2018-11-13 17:05:52 +0800

    zuul可用

--- csoptt_gateway-main/src/main/java/com/csoptt/CsopttGatewayMainApplication.java
+++ csoptt_gateway-main/src/main/java/com/csoptt/CsopttGatewayMainApplication.java
@@ -3,13 +3,16 @@ package com.csoptt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * 启动类
  * 可被注册中心发现
+ * 可作为网关
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class CsopttGatewayMainApplication {
 
 	public static void main(String[] args) {
